6081cde5cf942bb7daa730eb513f0e0c0cffe093
andrew-ware <andrew.ware@gordon.edu>
2016-10-17 23:24:24 -0400

    Add POST /users/login tests

--- test/user.js
+++ test/user.js
@@ -298,4 +298,228 @@ describe('Orders', () => {
 
   });
 
+  describe('POST /users/login + form', () => {
+
+    it('should return success and token if login successfully', (done) => {
+
+      const newAccount = {
+        id: 1,
+        account_name: 'CAT',
+        billing_address: '1 Main Street',
+        billing_city: 'main city',
+        billing_state: 'main state'
+      };
+
+      const password = 'password';
+
+      const newUser = {
+        first_name: 'Lebron',
+        last_name: 'James',
+        email: 'lj@cavs.com',
+        password: models.User.generateHash(password),
+        role: 1,
+        accountId: 1
+      };
+
+      models.Account
+        .create(newAccount)
+        .then((success) => {
+          models.User
+            .create(newUser)
+            .then((success) => {
+
+              const loginForm = {
+                email: newUser.email,
+                password
+              };
+
+              chai.request(app)
+                .post('/users/login')
+                .send(loginForm)
+                .end((err, res) => {
+                  if (err) {
+                    console.log(err.stack);
+                    throw err;
+                  }
+
+                  res.should.have.status(200);
+                  res.body.success.should.be.true;
+                  assert.typeOf(res.body.token, 'string');
+
+                  done();
+
+                });
+
+            })
+            .catch((err) => {
+              console.log(err.stack);
+              throw err;
+            });
+        })
+        .catch((err) => {
+          console.log(err.stack);
+          throw err;
+        });
+
+    });
+
+    it('should return failed and no token if wrong password or email', (done) => {
+
+      const newAccount = {
+        id: 1,
+        account_name: 'CAT',
+        billing_address: '1 Main Street',
+        billing_city: 'main city',
+        billing_state: 'main state'
+      };
+
+      const password = 'password';
+
+      const newUser = {
+        first_name: 'Lebron',
+        last_name: 'James',
+        email: 'lj@cavs.com',
+        password: models.User.generateHash(password),
+        role: 1,
+        accountId: 1
+      };
+
+      models.Account
+        .create(newAccount)
+        .then((success) => {
+          models.User
+            .create(newUser)
+            .then((success) => {
+
+              let loginForm = {
+                email: newUser.email,
+                password: 'refhuw'
+              };
+
+              chai.request(app)
+                .post('/users/login')
+                .send(loginForm)
+                .end((err, res) => {
+                  
+                  err.should.exist;
+                  res.should.have.status(403);
+                  res.body.success.should.be.false;
+                  assert.typeOf(res.body.token, 'undefined');
+                  res.body.message.should.contain('error');
+                  res.body.message.should.contain('password');
+
+                  loginForm.email = 'blah@blah.site'; // false email
+                  chai.request(app)
+                    .post('/users/login')
+                    .send(loginForm)
+                    .end((err, res) => {
+                      
+                      err.should.exist;
+                      res.should.have.status(403);
+                      res.body.success.should.be.false;
+                      assert.typeOf(res.body.token, 'undefined');
+                      res.body.message.should.contain('error');
+                      res.body.message.should.contain('email');
+
+                      done();
+
+                    });
+
+                });
+
+            })
+            .catch((err) => {
+              console.log(err.stack);
+              throw err;
+            });
+        })
+        .catch((err) => {
+          console.log(err.stack);
+          throw err;
+        });
+
+    });
+
+    it('should return failed if attempt to login without one of the two fields filled in', 
+      (done) => {
+
+      const newAccount = {
+        id: 1,
+        account_name: 'CAT',
+        billing_address: '1 Main Street',
+        billing_city: 'main city',
+        billing_state: 'main state'
+      };
+
+      const newUser = {
+        first_name: 'Lebron',
+        last_name: 'James',
+        email: 'lj@cavs.com',
+        password: 'password',
+        role: 1,
+        accountId: 1
+      };
+
+      models.Account
+        .create(newAccount)
+        .then((success) => {
+          models.User
+            .create(newUser)
+            .then((success) => {
+
+              let loginForm = {
+                email: undefined,
+                password: newUser.password
+              };
+
+              chai.request(app)
+                .post('/users/login')
+                .send(loginForm)
+                .end((err, res) => {
+                  
+                  err.should.exist;
+                  res.should.have.status(403);
+                  res.body.success.should.be.false;
+                  assert.typeOf(res.body.message, 'string');
+                  res.body.message.should.contain('error');
+                  res.body.message.should.contain('email');
+
+                  loginForm = {
+                    email: newUser.email,
+                    password: undefined
+                  };
+
+                  chai.request(app)
+                    .post('/users/login')
+                    .send(loginForm)
+                    .end((err, res) => {
+
+                      err.should.exist;
+                      res.should.have.status(403);
+                      res.body.success.should.be.false;
+                      assert.typeOf(res.body.message, 'string');
+                      res.body.message.should.contain('error');
+                      res.body.message.should.contain('password');
+
+                      done();
+
+                    });
+
+                });
+
+            })
+            .catch((err) => {
+              console.log(err.stack);
+              throw err;
+            });
+        })
+        .catch((err) => {
+          console.log(err.stack);
+          throw err;
+        });
+
+    });
+
+  });
+
 });
