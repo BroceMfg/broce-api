6b5b500393c21fec8d989c61ecab40bf89bf02bc
andrew-ware <andrew.ware@gordon.edu>
2016-10-25 13:36:39 -0400

    Add DELETE /users/{id}, TODO: add appropriate tests

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -5,53 +5,6 @@ const normalizeStringToInteger = require('./helpers/normalizeStringToInteger');
 const checkPermissions = require('./helpers/checkPermissions');
 const handleDBFindErrorAndRespondWithAppropriateJSON = require('./helpers/handleDBFindErrorAndRespondWithAppropriateJSON');
 
-// POST /users - anyone can access
-router.post('/', (req, res) => {
-
-  if (!req.body.first_name) return notProvidedFieldErrorResponse(res, 'first_name');
-  if (!req.body.last_name) return notProvidedFieldErrorResponse(res, 'last_name');
-  if (!req.body.email) return notProvidedFieldErrorResponse(res, 'email');
-  if (!req.body.password) return notProvidedFieldErrorResponse(res, 'password');
-  if (!req.body.role) return notProvidedFieldErrorResponse(res, 'role');
-  if (!req.body.accountId) return notProvidedFieldErrorResponse(res, 'accountId');
-
-  models.User
-    .findOne({
-      where: { email: req.body.email }
-    })
-    .then((user) => {
-
-      if (user)
-        return res.status(403).json({
-          success: false,
-          message: 'error: user already exists with that email'
-        });
-
-      models.User
-        .create({
-          first_name: req.body.first_name,
-          last_name: req.body.last_name,
-          email: req.body.email,
-          password: models.User.generateHash(req.body.password),
-          role: normalizeStringToInteger(req.body.role),
-          AccountId: req.body.accountId
-        })
-        .then((success) => {
-          return res.json({
-            success: true
-          });
-        })
-        .catch((err) => {
-          throw err;
-        });
-
-    })
-    .catch((err) => {
-      throw err;
-    });
-
-});
-
 // POST /users/login - anyone can access
 router.post('/login', (req, res) => {
 
@@ -118,6 +71,54 @@ router.get('/', (req, res) => {
 
 });
 
+// POST /users - anyone can access
+router.post('/', (req, res) => {
+
+  if (!req.body.first_name) return notProvidedFieldErrorResponse(res, 'first_name');
+  if (!req.body.last_name) return notProvidedFieldErrorResponse(res, 'last_name');
+  if (!req.body.email) return notProvidedFieldErrorResponse(res, 'email');
+  if (!req.body.password) return notProvidedFieldErrorResponse(res, 'password');
+  if (!req.body.role) return notProvidedFieldErrorResponse(res, 'role');
+  if (!req.body.accountId) return notProvidedFieldErrorResponse(res, 'accountId');
+
+  models.User
+    .findOne({
+      where: { email: req.body.email }
+    })
+    .then((user) => {
+
+      if (user)
+        return res.status(403).json({
+          success: false,
+          message: 'error: user already exists with that email'
+        });
+
+      models.User
+        .create({
+          first_name: req.body.first_name,
+          last_name: req.body.last_name,
+          email: req.body.email,
+          password: models.User.generateHash(req.body.password),
+          role: normalizeStringToInteger(req.body.role),
+          AccountId: req.body.accountId
+        })
+        .then((success) => {
+          return res.json({
+            success: true
+          });
+        })
+        .catch((err) => {
+          throw err;
+        });
+
+    })
+    .catch((err) => {
+      throw err;
+    });
+
+});
+
+// GET /users/{id} - only the user or admin can access
 router.get('/:id', (req, res) => {
 
   const id = normalizeStringToInteger(req.params.id);
@@ -142,5 +143,40 @@ router.get('/:id', (req, res) => {
   checkPermissions(req, res, null, id, cb);
 
 })
+
+router.delete('/:id', (req, res) => {
+
+  const id = normalizeStringToInteger(req.params.id);
+
+  const cb = () => {
+    models.User
+      .findOne({
+        where: {
+          id
+        }
+      })
+      .then((user) => {
+        user.destroy({ force: true })
+          .then((success) => {
+            res.json({
+              success: true
+            });
+          })
+          .catch((err) => {
+            console.log('error deleting user');
+            console.log(err.message);
+            throw err;
+          });
+
+      })
+      .catch((err) => {
+        console.log(err.message);
+        throw err;
+      });
+  }
+
+  checkPermissions(req, res, null, id, cb);
+
+});
 
 module.exports = router;
